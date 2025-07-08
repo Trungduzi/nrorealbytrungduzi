414b53847a7aed82d3717f5d12f46c6c01babb26
Trungduzi <vudangtrung2k5@gmail.com>
2025-07-09 01:47:38 +0700

    Sửa route và cấu hình CORS

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,14 +1,26 @@
 import express from "express";
 import cors from "cors";
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://nrorealbytrungduzi-production.up.railway.app"
+];
+
 const configViewEngine = (app) => {
     app.use(cors({
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
-        credentials: true
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
     }));
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-}
+};
 
 export default configViewEngine;

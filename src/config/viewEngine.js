c9d9dfe246348524e55cc04336b12715924a454f
Trungduzi <vudangtrung2k5@gmail.com>
2025-07-31 23:04:41 +0700

    sửa viewEngine 23:04 31/07/2025

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -7,11 +7,11 @@ const configViewEngine = (app) => {
         methods: ["GET", "POST"],
         credentials: true,
     }));
-    app.use(express.static(path.join(__dirname, 'build')));
+    // app.use(express.static(path.join(__dirname, 'build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
+    // app.get('*', (_req, res) => {
+    //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    // });
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));

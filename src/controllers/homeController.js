c1de31f6e1988006a4e43e634db6dbece750fffe
Trungduzi <vudangtrung2k5@gmail.com>
2025-08-16 00:17:02 +0700

    update lên web hôm 15/08/2025 22:03

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -85,7 +85,7 @@ export const createUser = async (req, res) => {
 
 const login = async (req, res) => {
     const { email, password } = req.body;
-
+    console.log(password);
     if (!email || !password) {
         return res.status(400).json({ message: "Không được để trống!" });
     }

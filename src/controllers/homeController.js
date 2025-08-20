9c4d37162d4937dcc7d5ec0e05270fac1fbe0459
Trungduzi <vudangtrung2k5@gmail.com>
2025-08-20 18:43:57 +0700

    update chức năng mua thẻ 16:32 20/08/2025

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -286,7 +286,7 @@ const getInformation = async (req, res) => {
 
 const addCardTable = async (idCard, idUser) => {
     const findCard = await db.createCard.findOne({ where: { idCard } });
-    const { name, price, code, serial, userid } = findCard;
+    const { name, price, code, serial } = findCard;
     await db.byCard.create({
         name,
         price,
